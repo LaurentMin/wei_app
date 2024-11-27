6db6b89912c6f7bb69cd85892e6655d55b2f6334
Laurent MINATCHY <laurent.minatchy@etu.utc.fr>
2024-11-27 22:56:12 +0800

    Ajustement de la bdd

--- lib/pages/product_page.dart
+++ lib/pages/product_page.dart
@@ -4,21 +4,27 @@ import '../widgets/floating_buttons.dart';
 class PageProduit extends StatelessWidget {
   final int idProduit;
   final String nameProduit;
+  final String ref;
+  final String size;
+  final String color;
 
   const PageProduit({
     super.key,
     required this.idProduit,
     required this.nameProduit,
+    required this.ref,
+    required this.size,
+    required this.color
   });
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Produit : $nameProduit'),
+        title: Text(nameProduit),
       ),
       body: Center(
-        child: Text('Détails du produit $nameProduit (ID : $idProduit)'),
+        child: Text('Détails du produit $nameProduit (ID : $idProduit)\n$size * $color * $ref'),
       ),
       floatingActionButton: const FloatingButtons(),
     );
