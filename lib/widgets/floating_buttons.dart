da13c2705db04944bf79db52984a7b56e43af181
Laurent MINATCHY <laurent.minatchy@etu.utc.fr>
2024-11-27 22:13:38 +0800

    Button to main page (again)

--- lib/widgets/floating_buttons.dart
+++ lib/widgets/floating_buttons.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../pages/user_page.dart';
 import '../pages/scan_page.dart';
+import '../pages/home_page.dart';
 
 class FloatingButtons extends StatelessWidget {
   const FloatingButtons({super.key});
@@ -9,7 +10,7 @@ class FloatingButtons extends StatelessWidget {
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        Positioned(
+        Positioned( // Users Button
           right: 20,
           bottom: 10,
           child: FloatingActionButton(
@@ -22,7 +23,7 @@ class FloatingButtons extends StatelessWidget {
             child: const Icon(Icons.account_circle),
           ),
         ),
-        Positioned(
+        Positioned( // Scan Button
           right: 90,
           bottom: 10,
           child: FloatingActionButton(
@@ -33,6 +34,19 @@ class FloatingButtons extends StatelessWidget {
               );
             },
             child: const Icon(Icons.add_circle),
+          ),
+        ),
+        Positioned( // Home Button
+          right: 160,
+          bottom: 10,
+          child: FloatingActionButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const MyHomePage()),
+              );
+            },
+            child: const Icon(Icons.folder),
           ),
         ),
       ],
