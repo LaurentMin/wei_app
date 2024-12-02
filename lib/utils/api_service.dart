307c9acfcab187807344b5ced24a07e315cd7efd
Laurent MINATCHY <laurent.minatchy@etu.utc.fr>
2024-12-02 11:41:27 +0800

    fonction to fetch an item by reference

--- lib/utils/api_service.dart
+++ lib/utils/api_service.dart
@@ -10,10 +10,26 @@ class ApiService {
     const String url = '$urlApi/items';
 
     try {
-      print("fetchItems running");
       final response = await http.get(Uri.parse(url));
       if (response.statusCode == 200) {
-        print("ApiFetch OK");
+        return jsonDecode(response.body);
+      } else {
+        print('Erreur : ${response.statusCode}');
+        return null;
+      }
+    } catch (e) {
+      print('Erreur lors de la connexion : $e');
+      return null;
+    }
+  }
+
+  // Rechercher un item par reference
+  static Future<List<dynamic>?> fetchItemRef(String refToFind) async {
+    String url = '$urlApi/items/$refToFind';
+
+    try {
+      final response = await http.get(Uri.parse(url));
+      if (response.statusCode == 200) {
         return jsonDecode(response.body);
       } else {
         print('Erreur : ${response.statusCode}');
